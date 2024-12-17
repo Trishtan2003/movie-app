07f517911ed8ea876dc4d9a9ba4c1089e8da52e1
Tristan Ervin G. Bartiquin <162120453+Trishtan2003@users.noreply.github.com>
2024-12-18 07:55:00 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MovieDetailPage from "./MovieDetailPage";
